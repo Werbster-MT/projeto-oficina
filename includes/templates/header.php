57268a8b217ceef2dd2aa7a56f2160a6154e3864
Werbster <wmteixeira.88@gmail.com>
2024-07-24 12:35:02 -0300

    [REFACT] Removendo duplicacao do bootstrap.bundle.min.js

--- includes/templates/header.php
+++ includes/templates/header.php
@@ -15,11 +15,8 @@
     <!-- Data Table -->
     <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
     <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
-    <script src="https://cdn.datatables.net/1.11.5/js/dataTables.bootstrap5.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-    
-    
 </head>
+<body>
 <header>
     <!-- Header nav menu -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark bg- p-3">
